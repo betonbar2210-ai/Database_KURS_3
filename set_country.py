29f49aef7d5927fc9a28fd4fed6e326cbaa83d63
Gluyki <betonbar2210@gmail.com>
2026-07-10 09:26:31 +0300

    видоизмененный проект с запросом от пользователя списка стран

--- set_country.py
+++ set_country.py
@@ -1,1 +1,4 @@
-country = ['Cuba', 'Senegal', 'Tunisia', 'England']
+answer_country = input("Введите интересующие страны через запятую\n")
+country_list = answer_country.split(',')
+country = [item.strip().capitalize() for item in country_list]
+
